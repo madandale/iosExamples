86c3aef7ba8987c66c97bc46b77beb6d8eb45fba
Madan Dale <madan_dale@Unicorns-MacBook-Pro.local>
2015-01-14 10:15:53 +0530

    Initial Commit

---
+++ AutoLayoutSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutSample
+//
+//  Created by Madan Dale on 14/01/15.
+//  Copyright (c) 2015 Madan Dale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AutoLayoutSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AutoLayoutSample
+//
+//  Created by Madan Dale on 14/01/15.
+//  Copyright (c) 2015 Madan Dale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
